296fb93f392c755a2061fc10edfdb03503ccf9fa
jang <lemona9201@gmail.com>
2024-03-29 19:15:33 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import data from './assets/portfolio.js';
+</script>
+
+<template>
+  <Navbar />
+  <router-view :data="data" />
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
